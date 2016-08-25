301b658ae2ff6b46ee2f28ca91e6d1cf11327bc4
willian.kirschner <willian.kirschner@NB-002842.cwinet.local>
2016-08-25 19:37:45 -0300

    método Tickets CREATE

---
+++ src/main/java/br/com/medvia/MessageReturn.java
@@ -0,0 +1,19 @@
+package br.com.medvia;
+
+/**
+ *
+ * @author willian.kirschner
+ */
+class MessageReturn {
+
+    private final String message;
+
+    public MessageReturn(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
--- src/main/java/br/com/medvia/TiketController.java
+++ src/main/java/br/com/medvia/TiketController.java
@@ -1,9 +1,12 @@
 package br.com.medvia;
 
+import br.com.medvia.resources.Ticket;
 import br.com.medvia.util.FakesTickets;
 import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class TiketController {
 
+    // OLD
     @RequestMapping("/api/tickets/list")
     public String getAllTickets(@RequestParam(value = "repeat", defaultValue = "1") int repeat) {
         System.out.println("repeat=" + repeat);
@@ -22,4 +26,16 @@ public class TiketController {
         return new Gson().toJson(fakesTickets.generate(repeat));
     }
 
+    @RequestMapping("/api/tickets")
+    public Ticket[] getAll(@RequestParam(value = "repeat", defaultValue = "1") int repeat) {
+        System.out.println("repeat=" + repeat);
+        FakesTickets fakesTickets = new FakesTickets();
+        return fakesTickets.generate(repeat);
+    }
+
+    @RequestMapping(path = "/api/tickets", method = RequestMethod.POST)
+    public MessageReturn create(@RequestBody String ticket) {
+
+        return new MessageReturn("Criou novo chamado com sucesso!");
+    }
 }
