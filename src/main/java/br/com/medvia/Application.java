82d760f48fed16ab0cde07c2ea44d5f891c68503
willian.kirschner <willian.kirschner@cwi.com.br>
2016-10-17 18:37:01 -0200

    implementando log

--- src/main/java/br/com/medvia/Application.java
+++ src/main/java/br/com/medvia/Application.java
@@ -2,21 +2,22 @@ package br.com.medvia;
 
 import br.com.medvia.db.WkDB;
 import java.io.File;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
  -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n
  java.io.tmpdir=C:\Users\willian.kirschner\AppData\Roaming\NetBeans\8.1\apache-tomcat-8.0.27.0_base\temp
- user.home=C:\Users\willian.kirschner
-
- java.io.tmpdir=/var/cache/tomcat8/temp
- user.home=/usr/share/tomcat8
  */
 @SpringBootApplication
 public class Application {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+//        System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %1$tb %1$td, %1$tY %1$tH:%1$tM:%1$tS %5$s%6$s%n");
         SpringApplication.run(Application.class, args);
     }
 
@@ -30,7 +31,7 @@ public class Application {
         File dirQC = new File(dir, "quality-control");
         dirQC.mkdir();
         QualityControlController.dirPDF = dirQC;
-                
+
         ServerController sc = new ServerController();
         sc.dbCreate();
     }
